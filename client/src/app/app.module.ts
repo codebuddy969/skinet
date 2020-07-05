991afae6769f54da2c4f6ad1cc6b536f3a71f13a
codebuddy969 <mymailboxmd@gmail.com>
2020-07-05 13:55:20 +0300

    End of section 10

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,18 +3,20 @@ import {NgModule} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
+import {AppRoutingModule} from "./app-routing.module";
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {CoreModule} from "./core/core.module";
-import {ShopModule} from "./shop/shop.module";
+import {HomeModule} from "./home/home.module";
 
 @NgModule({
     declarations: [AppComponent],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
+        AppRoutingModule,
         HttpClientModule,
         CoreModule,
-        ShopModule
+        HomeModule
     ],
     providers: [],
     bootstrap: [AppComponent]
